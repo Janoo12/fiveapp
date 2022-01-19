988f91740b9be6f48088e23941220b690d955c07
Janoo12 <janostamasi12@gmail.com>
2022-01-19 15:10:56 +0100

    Applying filtrations to our coding sessions

--- firstproject/codingsession/views.py
+++ firstproject/codingsession/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from codingsession.models import CodingSession
+from django.utils import timezone
+from django.utils.timezone import utc
+
 # Create your views here.
 
 def codingsessionspage(request):
-    coding_sessions = CodingSession.objects.all()
+    coding_sessions = CodingSession.objects.filter(starting_time__gte=timezone.now())
     return render(request, template_name="codingsession/all.html",
                   context={'coding_sessions' : coding_sessions}
                   )
